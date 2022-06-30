22ff21cf2436da16cedc72b390ff11ed3114bc38
Georgenduka <ndukageorgeveno@gmail.com>
2022-06-30 05:13:34 -0700

    print a string with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printing a string to standard output using 'puts'
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int main(void)
+{
+	puts("Programming is like buildig a multilingual puzzle");
+	return (0);
+}
