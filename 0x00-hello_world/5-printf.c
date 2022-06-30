b336cb0371b807d8351fcdfbb189f77d5e8c63bc
Georgenduka <ndukageorgeveno@gmail.com>
2022-06-30 06:03:41 -0700

    print out string with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
   * main- print string with print
   * Return: 0 if correct
   */
 int main(void)
 {
-	prinf("with proper grammar, but the outcome is a piece of art");
+	prinf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
