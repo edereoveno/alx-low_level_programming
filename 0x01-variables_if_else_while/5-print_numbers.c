cdbe908aaf1afd578c3f64c9a45d5b79994c5095
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-01 20:57:06 -0700

    prints out sinle digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - prints out single digits
+  * Return: returns 0
+  */
+
+int main(void)
+{
+int j;
+
+for (j = '0'; j <= '9'; j++)
+	{
+	printf("%d", j);
+	}
+	putchar('\n');
+	return (0);
+}
