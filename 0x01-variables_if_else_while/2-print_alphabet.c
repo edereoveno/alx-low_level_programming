32d846fe25296ee8ec4e5db4b04350992295bc51
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-01 20:04:00 -0700

    prints a to z in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - prints out alphabet in lowercase with putchar
+  * Return: returns 0
+  */
+
+int main(void)
+{
+char j;
+
+for (j = 'a'; j <= 'z'; j++)
+	{
+	putchar(j);
+	}
+	return (0);
+}
