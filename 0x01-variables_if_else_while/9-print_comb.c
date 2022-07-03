ffc07cdd7f859408de4b1d5cac7e559452b60f32
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-02 22:22:49 -0700

    prints out every combination of the alphabet

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+  * main - prints out every combination of alphabet
+  * Return: returns 0
+  */
+
+int main(void)
+{
+char j;
+char k;
+
+for (j = 'a'; j <= 'z'; j++)
+	{
+	for (k = 'a'; k <= 'z'; k++)
+		{
+		putchar(j);
+		putchar(k);
+		putchar(',');
+		putchar('\n');
+		}
+	}
+	return (0);
+}
