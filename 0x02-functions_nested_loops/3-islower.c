fd728181147f3fa865a4f5a485a463f0b04413a8
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-05 18:11:32 -0700

    returns 1 if lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+  * int _islower(int c) - checks if value is lowercase
+  * int main(void) - calls functiov
+  * Return: returns 1
+  */
+
+int main(void)
+{
+	int c;
+
+	int _islower(int c);
+}
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
