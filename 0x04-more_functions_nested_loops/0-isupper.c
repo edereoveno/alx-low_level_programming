4c1721cd21b7cb30048830b0dd264088cefa166a
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-07 19:32:30 +0000

    check for alphabet

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @i: variable
+ * Return: 1
+ */
+int _isupper(int c)
+{      
+	if (c >= 'A' && c<= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
