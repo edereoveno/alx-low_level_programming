4220b5242675f797627b75cebb396779f1b9fde3
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-09 12:55:56 +0000

    prints times table from 0 to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,15 +1,36 @@
 #include "main.h"
 
+/**
+ * times_table - prints timestable from 0 to 9
+ */
+
 void times_table(void)
 {
-	int a, b;
+	int a, b, c;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			_putchar(b);
-			_putchar('\n');
+			c = a * b;
+
+			if (c >= 0 && c < 10)
+			{
+				_putchar(c + '0');
+				if (b != 9)
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+			}
+			else if (c > 9 && c < 100)
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+				if (b != 9)
+					_putchar(',');
+					_putchar(' ');
+			}
 		}
+	_putchar('\n');
 	}
 }
