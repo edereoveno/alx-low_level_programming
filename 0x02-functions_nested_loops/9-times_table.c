067e3a2dfa7ab5e5af9df85feb16b6213c0585f0
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-09 13:00:50 +0000

    prints times table from 0 to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,17 +18,21 @@ void times_table(void)
 			{
 				_putchar(c + '0');
 				if (b != 9)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+				}
 			}
 			else if (c > 9 && c < 100)
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
 				if (b != 9)
+					{
 					_putchar(',');
 					_putchar(' ');
+					}
 			}
 		}
 	_putchar('\n');
