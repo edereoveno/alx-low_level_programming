ccfccbfa85c760074e5d24de2af9d7123cb4a4a6
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-10 17:26:56 +0000

    prints 50 fibinacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - calls fibonacci function
+ * Return: 0
+ */
+
 int main(void)
 {
 	fibonacci(50);
+	return (0);
 }
+
+/**
+ * fibonacci - prints sequence of addition of previous two numbers from 0 & 1
+ * @n: length of sequence
+ */
 
 void fibonacci(int n)
 {
