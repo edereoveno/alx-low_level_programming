c064fa507b83f8940183e6f14e9a8a2a6aa79aca
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-13 12:57:39 +0000

    print string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints string to stdout
+ * @str - pointer to string
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+}	
