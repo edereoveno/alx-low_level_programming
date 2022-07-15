13c1d37343ab7c9deb693cc6c1a8dd41ff3ca1b2
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-15 14:53:25 +0000

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,26 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0, j = 0;
 
-	for (i = 0; s2[i] != '\0' || s1[i] != '\0'; i++)
+/*	while (s1[i] != '\0' && s2[j] != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] != s2[j])
 		{
-			return (s1[i] - s2[i]);
+			return (s1[i] - s2[j]);
 		}
+		i++;
+		j++;
+	}
+	return (1);
+}
+*/	for (i = 0; s1[i] != '\0' || s2[j] != '\0'; i++)
+	{
+		if (s1[i] != s2[j])
+		{
+			return (s1[i] - s2[j]);
+		}
+		j++;
 	}
 	return (0);
 }
