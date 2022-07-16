27361bc7dcd18fd018ff9cffab85b4888297a52a
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-16 03:19:51 +0000

    encode to leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encode into leet
+ * @str: string to be encoded
+ * Return: string
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}
