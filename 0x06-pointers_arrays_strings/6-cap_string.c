31977cc0484117af34b9b888292743c262095fdb
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-16 02:16:00 +0000

    capitalizes first letter of each word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,11 +12,27 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '.')
+		/*if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '.') */
+		switch (str[i])
 		{
-			if ((str[i + 1] != ' ') && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
 			{
-				str[i + 1] = str[i + 1] - 32;
+				if ((str[i + 1] != ' ') && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+				{
+					str[i + 1] = str[i + 1] - 32;
+				}
 			}
 		}
 		i++;
