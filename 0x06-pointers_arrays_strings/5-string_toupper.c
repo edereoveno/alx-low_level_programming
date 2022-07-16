3192b1d8d81a2619eb8a33364078f7a178a81e76
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-16 01:02:47 +0000

    convertlowercase to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+/**
+ * string_toupper - coverts lowercase to upper
+ * @str: strig
+ * Return: char
+ */
+
 char *string_toupper(char *str)
 {
 	int i;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
