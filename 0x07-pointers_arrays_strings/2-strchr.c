f2a903fb633973a6fe46f9222d8a647975a0a62b
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-19 01:02:53 +0000

    checks for first occurence of character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,17 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0' ; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-		return (&s[i]);
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
 	}
-	return (0);
+	if (*s == c)
+	{
+		return (s);
+	}
+return (0);
 }
