56e6914bb40cc2b04e9ba2d76d1ebe54cdc1431f
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-20 02:10:56 +0000

    search for string in string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,11 +11,11 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i = 0, j;
 
-	for (; haystack[i]; i++)
+	for (; haystack[i] != '\0'; i++)
 	{
 		if (haystack[i] == needle[0])
 		{
-			for (j = 0; needle[j]; j++)
+			for (j = 0; needle[j] != '\0'; j++)
 			{
 				if (haystack[i + j] == needle[j])
 				{
