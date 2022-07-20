ab2c5c80a36fd164cdeb1e5b8a51f05ab5b7391f
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-20 03:08:22 +0000

    set value of pointer to char

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, sum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			sum += a[i][j];
+		}
+	}
+}
