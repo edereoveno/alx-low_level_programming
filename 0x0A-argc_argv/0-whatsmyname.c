1faf69e7c6e2c937249a4db914cb309c9b9e1f27
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-24 16:27:37 +0000

    print name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+
+
+int main(int argc, char **argv)
+{
+	printf("%s", argv[argc - 1]);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is a dir for practising Command line arguments in C
