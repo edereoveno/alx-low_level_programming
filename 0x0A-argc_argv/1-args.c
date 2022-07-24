818e5c0a0459689bf2736dda428ccb755fd73bd6
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-24 17:03:47 +0000

    prints out number of attributes

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints name of file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d", argc - 1);
+	putchar('\n');
+	return (0);
+}
