ae7d4e55d70d1e84fe7858538121400029e30f2c
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-26 15:31:42 +0000

    returns pointer to duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns pointer tp spring duplicate
+ * @str: pointer to string to be duplicated
+ * Return: pointer to duplicate
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i, len;
+
+	char *ar;
+
+	/* get length of string */
+	if (str == NULL)
+		return (NULL);
+	len = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	/* multiply length with size of char to get memory for duplicate string */
+	ar = malloc(sizeof(char) * len);
+
+	/* loop index and copy string */
+	for (i = 0; i < len; i++)
+	{
+		ar[i] = str[i];
+	}
+	return (ar);
+	free(ar);
+}
