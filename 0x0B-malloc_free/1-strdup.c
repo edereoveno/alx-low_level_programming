431074aba9f029baaf84f66345abfbff13c76248
Georgenduka <ndukageorgeveno@gmail.com>
2022-07-28 00:34:54 +0000

    concatenates two strings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -22,7 +22,7 @@ char *_strdup(char *str)
 		len++;
 	}
 	/* multiply length with size of char to get memory for duplicate string */
-	ar = malloc(sizeof(*str) * len);
+	ar = malloc(sizeof(char) * len);
 
 	if (ar == NULL)
 		return (NULL);
