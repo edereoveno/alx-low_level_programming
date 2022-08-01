5242dc020bc3c416a66b1f26b1e5a25c1a96ac8d
Georgenduka <ndukageorgeveno@gmail.com>
2022-08-01 14:02:01 +0000

    define dog  struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE dog.h
+/**
+ * struct dog - define dog structure with three elements
+ * @name: name of dog
+ * @age: age
+ * @owner: owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
