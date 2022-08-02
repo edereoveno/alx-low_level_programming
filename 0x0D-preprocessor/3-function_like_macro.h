3b4289e75a1431b22c09aa0197e8ca9d5746ca43
Georgenduka <ndukageorgeveno@gmail.com>
2022-08-02 02:42:47 +0000

    absolute value

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define ABS(x) ((x) > (0) ? (x) : -(x))
+#ifndef FUNCTION_LIKE_MACRO_H_
+#define FUNCTION_LIKE_MACRO_H_
+#define ABS(x) ((x) > (0) ? (x) : (-x))
 #endif
