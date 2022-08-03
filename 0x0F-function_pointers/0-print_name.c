6e195f4c1f7a47c1f069b5b80207b95a9a2598d2
Georgenduka <ndukageorgeveno@gmail.com>
2022-08-03 23:44:53 +0000

    prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: pointer to char
+ * @f: pointer to function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef FUNCTION_POINTERS_H_
+#define FUNCTION_POINTERS_H_
+
+void print_name(char *name, void (*f)(char *));
+#endif
