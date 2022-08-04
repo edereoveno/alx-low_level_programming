d3c3bf2dbbf01d2c922f3ef26a54fa25d7df5638
Georgenduka <ndukageorgeveno@gmail.com>
2022-08-04 04:24:42 +0000

    hard stuff

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,22 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - function to select correct operation function
+ * @s: operation given (+, *, -, /, %)
+ * Return: pointer to correct operation function (0-4)
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t oper[] = {{"+", op_add}, {"-", op_sub}, {"*", op_mul}, {"/", op_div}, {"%", op_mod}, {NULL, NULL}};
+
+	int i = 0;
+
+	while (oper[i] != NULL)
+	{
+		if (*s == *oper[i].op)
+			return *oper[i].f;
+		i++;
+	}
+	return (NULL):
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds arguments
+ * @a: argument
+ * @b: argument
+ * Return: returns sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substracts arguments
+ * @a: argument
+ * @b: argument
+ * Return: returns diff
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies arguments
+ * @a: argument
+ * @b: argument
+ * Return: returns product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides arguments
+ * @a: argument
+ * @b: argument
+ * Return: returns division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns remainder of arguments
+ * @a: argument
+ * @b: argument
+ * Return: returns remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
