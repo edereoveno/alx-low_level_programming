2dcc5f675e007fc3d9cd875ffbdbe2bddd7efbf1
Georgenduka <ndukageorgeveno@gmail.com>
2022-08-20 23:50:08 +0000

    print binary of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,22 +7,7 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int output;
-	int i, count = 0;
-
-
-	for (i = 63; i >= 0; i--)
-	{
-		output = n >> i;
-		if (output & 1)
-		{
-			_putchar('1');
-			count++;
-		}
-
-		else if (count)
-			_putchar('0');
-	}
-	if (!count)
-		_putchar('0');
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar('0' + (n & 1));
 }
