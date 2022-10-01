53e4231077b3e3778bed88aea569210a115b913d
georgenduka <ndukageorgeveno@gmail.com>
2022-09-30 18:04:07 -0700

    crackme

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,1 +1,12 @@
-en C Pyfo neZ
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * keygen print a valid key for the username
+ * Return: key of the username
+ **/
+
+int main()
+{
+  return (key);
+}
