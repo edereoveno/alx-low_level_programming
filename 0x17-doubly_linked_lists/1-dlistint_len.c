53b263fd0f002c528460f23906892a90ff62428d
georgenduka <ndukageorgeveno@gmail.com>
2022-09-30 17:02:51 -0700

    return number of elements

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,6 +2,12 @@
 #include <stddef.h>
 #include "lists.h"
 
+/**
+ * dlistint_len - returns the number of elements
+ * @h: pointer to head
+ * Return: number of elements
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
@@ -11,5 +17,6 @@ size_t dlistint_len(const dlistint_t *h)
 		count++;
 		h = h->next;
 	}
+
 	return (count);
 }
